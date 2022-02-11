de1baba8a459db8a65d7aa14732ba4bc42fcd8f1
Zhenya Horlach <gorlachjob@ukr.net>
2022-02-11 10:44:13 +0200

    Add new changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import User from './components/User';
-import users from './user.json';
+import user from './user.json';
+import Statistics from './components/Statistics';
+import data from './data.json';
+import FriendsList from './components/FriendsList';
+import friends from './friends.json';
+import TransactionsList from './components/TransactionsList';
+import transactions from './transactions.json';
 
 export default function App() {
     return (
         <div>
             <User
-                tag={users[0].tag}
-                username={users[0].username}
-                location={users[0].location}
-                avatar={users[0].avatar}
-                userFollowers={users[0].stats.followers}
-                userViews={users[0].stats.views}
-                userLikes={users[0].stats.likes}
+                tag={user.tag}
+                username={user.username}
+                location={user.location}
+                avatar={user.avatar}
+                userFollowers={user.stats.followers}
+                userViews={user.stats.views}
+                userLikes={user.stats.likes}
             />
-            {/* <Data
-                label={data[0].label}
-                percentage={data[0].percentage}
-            /> */}
-            {/* <Friends
-                avatar={friends[0].avatar}
-                name={friends[0].name}
-
-            /> */}
-            {/* <Transactions
-                type={transactions[0].type}
-                amount={transactions[0].amount}
-                currency={transactions[0].currency}
-            /> */}
+            <Statistics
+                title="Upload stats"
+                stats={data}
+            />
+            <FriendsList friends={friends} />
+            <TransactionsList transactions={transactions} />
         </div>
     );
 }
